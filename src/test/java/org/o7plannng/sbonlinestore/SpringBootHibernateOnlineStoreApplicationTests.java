e8228b8b2a9ece078692d825f307f1d274ab4043
someshsb1 <someshbalanisb@gmail.com>
2023-05-18 10:39:09 -0400

    Initial commit

---
+++ src/test/java/org/o7plannng/sbonlinestore/SpringBootHibernateOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7plannng.sbonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
